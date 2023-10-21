f1ac094d2dda73e7c51502cb29fbd6437201c302
YukiMaitani <fightzoi1997@gmail.com>
2023-10-22 08:04:19 +0900

    APIを叩いた時にstateに応じて処理を分けられるようにする

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { getWeatherData } from './weatherAPI.js';
+import { fetchWeatherData } from './weatherAPI.js';
+
+function updateUI(state) {
+  if (state.loading) {
+      console.log("Loading...");
+  } else if (state.data) {
+      console.log(state.data);
+  } else if (state.error) {
+      console.log(`Error: ${state.error}`);
+  }
+}
 
 async function main() {
-    const latitude = 35.6895;
-    const longitude = 139.6917;
-
-    const weatherInfo = await getWeatherData(latitude, longitude);
-    console.log(weatherInfo);
+  //await fetchWeatherData(35.6895, 139.6917, updateUI);
 }
 
 main();
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,16 +1,37 @@
 import { API_KEY } from './config.js';
 
+const state = {
+  loading: false,
+  data: null,
+  error: null,
+};
+
 async function getWeatherData(latitude, longitude) {
-    const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,daily,alerts&appid=${API_KEY}`;
-
-    const response = await fetch(endpoint);
-    const data = await response.json();
-
-    return {
-        temperature: data.current.temp,
-        weather: data.current.weather[0].description,
-        timezone: data.timezone
-    };
+  const endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,daily,alerts&appid=${API_KEY}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return {
+      temperature: data.current.temp,
+      weather: data.current.weather[0].description,
+      timezone: data.timezone
+  };
 }
 
-export { getWeatherData };
+async function fetchWeatherData(latitude, longitude, updateUI) {
+  state.loading = true;
+  state.data = null;
+  state.error = null;
+  updateUI(state);
+  try {
+      const weatherInfo = await getWeatherData(latitude, longitude);
+      state.loading = false;
+      state.data = weatherInfo;
+  } catch (error) {
+      state.loading = false;
+      state.error = error.message;
+  }
+
+  updateUI(state);
+}
+
+export { fetchWeatherData };
