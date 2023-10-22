0b4d43a7ddcb417129f63ba4f54ecf56b71b6777
YukiMaitani <fightzoi1997@gmail.com>
2023-10-22 09:51:58 +0900

    ファイル名変更 weatherAPI → weather_api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
-import { fetchWeatherData, fetchMockWeatherData } from './weatherAPI.js';
+import { fetchWeatherData, fetchMockWeatherData } from './weather_api.js';
 
 let camera, scene, renderer, font, mesh;
 
